e44d4efdadd06441c44abb1ef24c2fb7fbbed3f9
Ujjwal Pawar <s2325287@ed.ac.uk>
2023-03-06 17:03:45 +0000

    [ujjwal] modified enb execute file

--- execute_enb12.sh
+++ execute_enb12.sh
@@ -1,20 +1,12 @@
-PATHTOOAI=~/openairinterface5g/
-PATHTOPROXY=~/oai-lte-5g-multi-ue-proxy/
-
-folder=$1
-
-if [ "$folder" = "" ]; then
-  echo "Missing the folder to save the logs"
-  exit 1
-fi
-
+#PATHTOOAI=~/openairinterface5g/
+PATHTOOAI=~/openairinterface-nfapi-netsys/
 
 echo "Executing the eNBs"
 cd $PATHTOOAI
 source oaienv
 cd cmake_targets/
 
-sudo -E ./ran_build/build/lte-softmodem -O ~/enb.conf \
+sudo -E ./ran_build/build/lte-softmodem -O ~/enb.conf $2 $3 \
   --node-number 1 --emulate-l1 \
- --log_config.global_log_options level,nocolor,time,thread_id --log_config.global_log_level debug | tee ~/$folder/eNB1.log 2>&1
+ --log_config.global_log_options level,nocolor,time,thread_id --log_config.global_log_level $1 | tee ~/enb1.log 2>&1
 
