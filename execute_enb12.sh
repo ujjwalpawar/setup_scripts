8a00d65a2d929ea60e584f6eb056263cd1be0660
Ujjwal Pawar <uzzu@node0.uzzu-149866.test5g.emulab.net>
2023-03-20 10:20:25 -0600

    modified conf files

--- execute_enb12.sh
+++ execute_enb12.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #PATHTOOAI=~/openairinterface5g/
 PATHTOOAI=~/openairinterface-nfapi-netsys/
 
@@ -5,8 +6,10 @@ echo "Executing the eNBs"
 cd $PATHTOOAI
 source oaienv
 cd cmake_targets/
+sudo tcpdump -i vlan134 -w ~/enb1.pcap &
 
 sudo -E ./ran_build/build/lte-softmodem -O ~/enb.conf $2 $3 \
   --node-number 1 --emulate-l1 \
  --log_config.global_log_options level,nocolor,time,thread_id --log_config.global_log_level $1 | tee ~/enb1.log 2>&1
 
+sudo pkill -9 -f tcpdump
