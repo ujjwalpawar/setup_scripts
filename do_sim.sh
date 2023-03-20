ce7ee39ce6037c66fdf9f6c11b57ee950c70f8a1
Ujjwal Pawar <uzzu@node4.uzzu-149866.test5g.emulab.net>
2023-03-20 10:23:24 -0600

    ue files updated

--- do_sim.sh
+++ do_sim.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-cp -a sim.conf openairinterface5g/cmake_targets/ran_build/build/
-cd openairinterface5g/cmake_targets/ran_build/build/
+cp -a sim.conf ~/openairinterface5g/cmake_targets/ran_build/build/
+cd ~/openairinterface5g/cmake_targets/ran_build/build/
 ../../../targets/bin/conf2uedata -c sim.conf -o .
 ../../../targets/bin/usim -g -c sim.conf -o .
 ../../../targets/bin/nvram -g -c sim.conf -o .
--- execute_ue_s1.sh
+++ execute_ue_s1.sh
@@ -9,4 +9,4 @@ source oaienv
 cd cmake_targets/ran_build/build/
 sudo -E ./lte-uesoftmodem -O ../../../ci-scripts/conf_files/episci/proxy_ue.nfapi.conf \
  --L2-emul 5 --nokrnmod 1 --num-ues 1 --node-number 1 --num-enbs 2 \
- --log_config.global_log_options level,nocolor,time,thread_id --log_config.global_log_level $1 $2 $3 | tee ~/ue.log 2>&1 # &> ~/$folder/UE.log  
+ --log_config.global_log_options level,nocolor,time,thread_id --log_config.global_log_level error $1 $2 $3 | tee ~/ue.log 2>&1 # &> ~/$folder/UE.log  
